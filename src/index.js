2d7ef3e1d50a2135d91f33842b6e00bbff5cb1b0
Pedro Henrique Martins e Oliveira <pedrogmhead@gmail.com>
2020-09-09 08:17:15 -0300

    reorganized files inside the project better organization

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
